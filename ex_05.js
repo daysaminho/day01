0fb1f17140bddd08ce11c44efc2aaee3651d5da3
daysaminho <daysam.moussaoui@epitech.digital>
2024-09-30 13:21:53 +0200

    Create ex_05.js

---
+++ ex_05.js
@@ -0,0 +1,8 @@
+let firstname = ("John") 
+let username = "Doe"
+let cardCode = "1111 2222 3333 4444"
+let expiryDateMonth = 21
+let expiryDateYear = 2042
+
+let computedBankInfo = firstname+","+username+","+cardCode+","+expiryDateMonth+"/"+expiryDateYear
+displayInCard(computedBankInfo)

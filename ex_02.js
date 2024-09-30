92ce0ff2d6992a2d85f270a9b279952584e57059
daysaminho <daysam.moussaoui@epitech.digital>
2024-09-30 16:37:49 +0200

    Update ex_02.js

--- ex_02.js
+++ ex_02.js
@@ -5,7 +5,7 @@ let float = 42.42
 let this_is_null = null
 
 
-document.write(bool + "<br/>")
-document.write(string + "<br/>")
-document.write(integer + "<br/>")
-document.write(float)
+displayThisText(bool + "<br/>")
+displayThisText(string + "<br/>")
+displayThisText(integer + "<br/>")
+displayThisText(float)

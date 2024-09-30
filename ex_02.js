49e2b3c8f2a6a6337f66fd5b531de9cff3a68e08
daysaminho <daysam.moussaoui@epitech.digital>
2024-09-30 11:05:27 +0200

    Create ex_02.js

---
+++ ex_02.js
@@ -0,0 +1,11 @@
+let bool = true
+let string = "forty two"
+let integer = 42
+let float = 42.42
+let this_is_null = null
+
+
+document.write(bool + "<br/>")
+document.write(string + "<br/>")
+document.write(integer + "<br/>")
+document.write(float)

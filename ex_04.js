0e9805d719b19d6559ababc2c7c5ea323ae91ad0
daysaminho <daysam.moussaoui@epitech.digital>
2024-09-30 11:57:43 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,14 @@
+let w;
+document.write(typeof(w) + "<br/>");
+let bool= true 
+document.write(typeof(bool) + "<br/>");
+let y = 29
+document.write(typeof(y) + "<br/>")
+let u = 72698374983408304934048n
+document.write(typeof(u) + "<br/>")
+let monsymbol = Symbol("piscine")
+document.write(typeof(monsymbol) + "<br/>")
+let objet = {}
+document.write(typeof(objet) + "<br/>")
+let fonction = Function("test")
+document.write(typeof(fonction) + "<br/>")
